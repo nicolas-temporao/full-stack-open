89c49b539b179e56fbc05457778db7da27dd5b7d
nicolas-temporao <nicolastemporao@gmail.com>
2026-09-03 04:17:52 -0400

    up to 3.17

--- part3/phonebackend/models/person.js
+++ part3/phonebackend/models/person.js
@@ -4,9 +4,25 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(url)
+
+console.log('Connecting to MongoDB...')
+mongoose.connect(url, { family: 4 })
+    .then(() => {
+        console.log('Connected.')
+    })
+    .catch(error => {
+        console.log('error connecting:', error.message)   
+    })
 
 const personSchema = new mongoose.Schema({ name: String, number: String })
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 const Person = mongoose.model('Person', personSchema) 
 
