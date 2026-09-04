ee47283813b70ad52c363c183d67c52fe0ee8c4c
nicolas-temporao <nicolastemporao@gmail.com>
2026-09-04 01:36:59 -0400

    part 3 done

--- part3/phonebackend/eslint.config.mjs
+++ part3/phonebackend/eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ['**/*.js}'],
+    files: ['**/*.js'],
     plugins: {
       js,
       '@stylistic/js': stylisticJs
