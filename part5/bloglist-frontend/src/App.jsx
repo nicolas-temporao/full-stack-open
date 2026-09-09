ca3398efb71b717ccc8ae16ca9fcc56ffb5f957c
nicolas-temporao <nicolastemporao@gmail.com>
2026-09-09 02:26:40 -0400

    part 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,15 +1,46 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try { 
+      const loggedUser = await loginService.login({username, password})
+
+      setUser(loggedUser)
+      setUsername('')
+      setPassword('')
+
+    } catch (error){
+      console.log('wrong credentials')
+    }
+  }
+
+    
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm username={username} password={password} handleUserChange={({target}) => setUsername(target.value)} handlePassChange={({target}) => setPassword(target.value)} handleLogin={handleLogin} />
+      </div>
+    )
+  }
+
+
 
   return (
     <div>
---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+const LoginForm = ({username, password, handleUserChange, handlePassChange, handleLogin}) => {
+
+   return ( 
+        <div>
+            <form onSubmit={handleLogin}>
+            <div>
+                <label>
+                username
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleUserChange}
+                />
+                </label>
+            </div>
+            <div>
+                <label>
+                password
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePassChange}
+                />
+                </label>
+            </div>
+            <button type="submit">login</button>
+            </form>
+        </div>
+    ) 
+}     
+      
+export default LoginForm
